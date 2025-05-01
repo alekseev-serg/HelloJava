16f3c98d8e8b5dd1c98226e3cd6405ef1079c82a
sergey.alekseev <demiurg58@gmail.com>
2025-05-01 10:09:02 +0300

    add one lesson

--- src/conditions/Conditions.java
+++ src/conditions/Conditions.java
@@ -11,19 +11,24 @@ public class Conditions {
 
         int x = 25;
         int scale = 3;
+        int score;
         if(x > 10){
-            int score = 3 * scale;
+            score = 3 * scale;
         } else {
-            int score = 4 * scale;
+            score = 4 * scale;
         }
+        System.out.println(score);
 
         int income = 25;
-
+        double tax;
         if(income > 10000){
-            double tax = income * 0.2;
+            tax = income * 0.2;
         }else{
-            double tax = income * 0.17 + 1000;
+            tax = income * 0.17 + 1000;
         }
+        System.out.println(tax);
+
+
         int number = 24;
         double i = 50, j = 45;
         System.out.println((number % 3 == 0) ? i : j);
