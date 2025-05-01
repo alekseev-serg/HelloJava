3cc08758f123dce70a5818f913f8f423647a77b3
sergey.alekseev <demiurg58@gmail.com>
2025-05-01 10:48:08 +0300

    test done

--- src/Test.java
+++ src/Test.java
@@ -1,6 +1,32 @@
 public class Test {
     public static void main(String[] args) {
-        int n = 10000 * 10000 * 10000;
-        System.out.println("n равно " + n);
+        int x = 15;
+        int y = -1;
+        int z = 1;
+
+        if (x > 0){
+            if(y > 0){
+                System.out.println("'x > 0 and y > 0'");
+            }
+        } else if (z > 0){
+            System.out.println("'x < 0 and z > 0'");
+        }
+
+        if((x < 100) && (x > 10))
+            System.out.println("x between 10 and 100");
+            
+        int number = 10;
+        boolean even;
+        even = number % 2 == 0;
+        System.out.println(even);
+
+        int q = 1;
+        int j = q = q + 1;
+        System.out.println(j);
+
+        boolean ev = false;
+        if (ev = true){
+            System.out.println(ev);
+        }
     }
 }
