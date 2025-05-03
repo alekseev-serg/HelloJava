c14fe0958a3c90eef2ec79d5166c99665f6a40b4
sergey.alekseev <demiurg58@gmail.com>
2025-05-03 16:25:59 +0300

    add math and string lessons

--- src/Test.java
+++ src/Test.java
@@ -1,32 +1,75 @@
 public class Test {
     public static void main(String[] args) {
-        int x = 15;
-        int y = -1;
-        int z = 1;
+        // int x = 0;
 
-        if (x > 0){
-            if(y > 0){
-                System.out.println("'x > 0 and y > 0'");
+        // while (x < 4) {
+        //     x += 1;
+        // }
+        // System.out.println(x);
+
+        // int count = 0;
+        // while (count++ < 10) {
+        //     System.out.println(count + ": Welocme Java");
+        // }
+        // int count = 0;
+        // do {
+        //     
+        // } while(count++ < 9);
+        // System.out.println(count);
+
+        // for(int count = 1; count <= 10; count++){
+        //     System.out.println(count + ": Welocme Java");
+        // }
+
+        // for(int count = 0; count < 10; count++){
+        //     System.out.println(count + ": Welocme Java");
+        // }
+
+        // for(int count = 1; count < 10; count++){
+        //     System.out.println(count + ": Welocme Java");
+        // }
+
+        // for(int count = 0; count <= 10; count++){
+        //     System.out.println(count + ": Welocme Java");
+        // }
+
+        // for (int i = 1; i <= 10; i++){
+        //     System.out.print(i + " ");
+        //     i++;
+        // }
+
+        // int i = 0;
+        // for (i = 1; i <= 10; i++);
+        //     System.out.println(i + 4);
+
+        // for(int i = 1; i <= 6; i++){
+        //     for(int j = 6; j >= 1; j--){
+        //         System.out.print(j <= i ? j + " " : " " + " ");
+        //     }
+        //     System.out.println();
+        // }
+        // int count = 1;
+        // for(int i = 0; i < 10; i++)
+        //     for(int j = 0; j < i; j++)
+        //         System.out.println(i * j);
+        //     count+=1;
+        // System.out.println(count);
+
+        // double sum = 0;
+        // for (double d = 0; d < 10; sum += sum + d){
+        //     d += 0.1;
+        // }
+
+        int number = 25;
+        int i;
+
+        boolean isPrime = true;
+        for (i = 2; i < number && isPrime; i++){
+            if(number % i == 0){
+                isPrime = false;
             }
-        } else if (z > 0){
-            System.out.println("'x < 0 and z > 0'");
         }
 
-        if((x < 100) && (x > 10))
-            System.out.println("x between 10 and 100");
-            
-        int number = 10;
-        boolean even;
-        even = number % 2 == 0;
-        System.out.println(even);
-
-        int q = 1;
-        int j = q = q + 1;
-        System.out.println(j);
-
-        boolean ev = false;
-        if (ev = true){
-            System.out.println(ev);
-        }
+        System.out.println("i = " + i + " isPrime = " + isPrime);
     }
 }
